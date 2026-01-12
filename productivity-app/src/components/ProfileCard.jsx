f41dcf02a87d6631602d0a2de0de7ca0e8322ece
Marcus <marcuswilliamj@gmail.com>
2026-01-12 14:25:32 +0100

    Added test card

--- productivity-app/src/components/ProfileCard.jsx
+++ productivity-app/src/components/ProfileCard.jsx
@@ -1,8 +1,17 @@
 import React from "react";
-import "./components/ProfileCard.css";
+import "./ProfileCard.css";
 
 const ProfileCard = () => {
-  return <div>ProfileCard</div>;
+  return (
+    <div>
+      <img src="https://i.pravatar.cc/300" alt="Profile image" />
+      <h2>Profile Card</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod
+        voluptatem aut cumque rerum, odit cum a repellendus vero ratione eos.
+      </p>
+    </div>
+  );
 };
 
 export default ProfileCard;
