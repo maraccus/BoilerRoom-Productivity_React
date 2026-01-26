2cdbd93b85cb3fcc1db22f96fb2344af43645b31
Marcus <marcuswilliamj@gmail.com>
2026-01-26 14:00:44 +0100

    Added nav icons

--- productivity-app/src/components/Navigation.jsx
+++ productivity-app/src/components/Navigation.jsx
@@ -1,11 +1,27 @@
 import React from "react";
 import styles from "./Navigation.module.css";
+import NavigationSpacer from "./NavigationSpacer";
 
 import GearIcon from "../assets/gear-solid-full.svg?react";
+import CalendarIcon from "../assets/calendar-regular-full.svg?react";
+import ChartIcon from "../assets/chart-simple-solid-full.svg?react";
+import ClockIcon from "../assets/clock-regular-full.svg?react";
 
 const Navigation = () => {
   return (
     <nav className={styles.navBar}>
+      <button className={styles.iconBtn} aria-label="Settings">
+        <ClockIcon className={styles.icon} />
+      </button>
+      <button className={styles.iconBtn} aria-label="Settings">
+        <CalendarIcon className={styles.icon} />
+      </button>
+      <button className={styles.iconBtn} aria-label="Settings">
+        <ChartIcon className={styles.icon} />
+      </button>
+
+      <NavigationSpacer />
+
       <button className={styles.iconBtn} aria-label="Settings">
         <GearIcon className={styles.icon} />
       </button>
---
+++ productivity-app/src/components/NavigationSpacer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from "./NavigationSpacer.module.css";
+
+
+const NavigationSpacer = () => {
+  return (
+    <div className={styles.spacer}></div>
+  );
+};
+
+export default NavigationSpacer;
