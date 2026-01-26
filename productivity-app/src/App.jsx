a2247287a13888ecb4e7cda61cf3883834f5c36c
Marcus <marcuswilliamj@gmail.com>
2026-01-26 14:13:36 +0100

    Added footer component

--- productivity-app/src/App.jsx
+++ productivity-app/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import MainContainer from "./components/MainContainer"
 import ProfileCard from "./components/ProfileCard";
 import Navigation from "./components/Navigation";
+import Footer from "./components/Footer";
 import "./App.css";
 
 function App() {
@@ -10,7 +11,7 @@ function App() {
       <MainContainer>
         <Navigation/>
         {/*Add main components here*/}
-        
+        <Footer/>
       </MainContainer>
     </>
   );
