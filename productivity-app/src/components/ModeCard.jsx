487db7b3a6cac8277bbaf556132659015436409f
Tomac Barin Jansson <tomacbarin@me.com>
2026-02-04 14:50:07 +0100

    Fixed Icon-prop-issue.

--- productivity-app/src/components/ModeCard.jsx
+++ productivity-app/src/components/ModeCard.jsx
@@ -2,7 +2,9 @@
 import React from "react";
 import styles from "./ModeCard.module.css";
 
+// NOTERA: Icon-proppen fungerar! Gråmarkeringen är troligen en bugg eller ESLint-tjosan. Ignorera.
 export default function ModeCard({ title, Icon, onClick }) {
+  // console.log("Detta är Icon från ModeCard:", Icon);
   return (
     <button className={styles.modeCard} onClick={onClick}>
       <Icon className={styles.modeIcon} />
