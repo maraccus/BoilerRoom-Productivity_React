4110e996155dedb13af85fc5a3362d058f01963f
Marcus <marcuswilliamj@gmail.com>
2026-02-04 13:48:59 +0100

    Added current day

--- productivity-app/src/components/GraphWeekday.jsx
+++ productivity-app/src/components/GraphWeekday.jsx
@@ -4,8 +4,13 @@ import styles from "./GraphWeekday.module.css";
 const GraphWeekday = () => {
   return (
     <section className={styles.container}>
-        <h2 className={styles.containerH2}>Tuesday</h2>
-        <h2 className={styles.containerH3}>January 26</h2>
+        <h2 className={styles.containerH2}>{new Date().toLocaleDateString("en-US", {
+    weekday: "long",
+  })}</h2>
+        <h2 className={styles.containerH3}>{new Date().toLocaleDateString("en-US", {
+    month: "long",
+    day: "numeric",
+  })}</h2>
 
         {Array.from({ length: 24 }).map((_, hour) => (
             <div className={styles.containerDay}>
@@ -19,6 +24,7 @@ const GraphWeekday = () => {
                 </div>
             </div>
         ))}
+        <div className={styles.dividerLine}></div>
 
     </section>
   )
