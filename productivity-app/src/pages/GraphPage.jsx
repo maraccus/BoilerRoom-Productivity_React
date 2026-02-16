5fd05b4a4e37de7f9ef754e0258c4db250b86fbd
Marcus <marcuswilliamj@gmail.com>
2026-02-16 15:47:03 +0100

    Fixed recharts problem (still broken)

--- productivity-app/src/pages/GraphPage.jsx
+++ productivity-app/src/pages/GraphPage.jsx
@@ -4,12 +4,17 @@ import ContainerV from "../components/ContainerV";
 import ContainerH from "../components/ContainerH";
 import Navigation from "../components/Navigation";
 import Footer from "../components/Footer";
+import BarChart from "../components/BarChart"
 
 const GraphPage = () => {
   return (
     <MainContainer>
         <Navigation/>
         <h1>Graphs Page</h1>
+        <ContainerV>
+        <BarChart/>
+
+        </ContainerV>
         <Footer/>
     </MainContainer>
   )
